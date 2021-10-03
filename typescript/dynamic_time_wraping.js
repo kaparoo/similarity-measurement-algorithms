31a03acb452fa7e32c8bb208310a00235919c9a2
kaparoo <kaparoo2001@gmail.com>
2021-10-03 16:45:42 +0900

    :pencil2: Fix typos

--- typescript/dynamic_time_wraping.js
+++ typescript/dynamic_time_wraping.js
@@ -1,6 +1,6 @@
-function makeMatrix(numRaws, numCols, initialValue = 0) {
-    let matrix = new Array(numRaws);
-    for (let i = 0; i < numRaws; ++i) {
+function makeMatrix(numRows, numCols, initialValue = 0) {
+    let matrix = new Array(numRows);
+    for (let i = 0; i < numRows; ++i) {
         matrix[i] = new Array(numCols)
         for (let j = 0; j < numCols; ++j) {
             matrix[i][j] = initialValue;
@@ -77,8 +77,8 @@ function run(seq1, seq2) {
 
 function displayMatrix(matrix) {
     matrix.reverse();
-    matrix.forEach(raw => {
-        raw.forEach(element => process.stdout.write(`[${element}]`));
+    matrix.forEach(row => {
+        row.forEach(element => process.stdout.write(`[${element}]`));
         console.log();
     });
     matrix.reverse();
